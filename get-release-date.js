5c9c3d4246f8296d50969808737a35c24a786df7
wei <leohoo@hotmail.com>
2024-11-12 09:43:41 +0900

    fix version string regex

--- get-release-date.js
+++ get-release-date.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
   await page.goto('https://developer.dji.com/doc/mobile-sdk-tutorial/en/', { waitUntil: 'networkidle2' });
   const updateDate = await page.$eval('.update-date', el => el.innerText);
   const h2 = await page.$eval('h2', el => el.innerText);
-  const version = h2.match(/V\d+\.\d+\.\d+/)[0];
+  const version = h2.match(/[Vv]\d+\.\d+\.\d+/)[0];
   console.log(version, updateDate);
   const fs = require('fs');
   fs.writeFileSync('current-version.txt', version);
